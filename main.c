9a6fb56494316ce76e5271579cb0394a201fa34c
Lightstrap <91497289+LightstrapRBX@users.noreply.github.com>
2023-02-24 23:58:23 +0000

    fixes and changes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX_LENGTH 30
 
@@ -8,7 +9,8 @@ struct person {
     char fullName[MAX_LENGTH];
     char firstName[MAX_LENGTH];
     char lastName[MAX_LENGTH];
-    int wallet;
+
+    int cash;
 };
 
 struct bankAccount {
@@ -38,18 +40,57 @@ int main() {
     strcat((personPtr)->fullName, (personPtr)->lastName);
     
     printf("\nHello, %s! A bank account will be registered under your name.\n", (personPtr)->fullName);
-    
-    fflush(stdin);
-    while (getchar() != '\n');
-    getchar();
-    
+
+    while (clock() < 1000 * 1000);
     printf("Starting registration...\n");
     
     struct bankAccount *accountPtr;
     accountPtr = (struct bankAccount*) malloc(1 * sizeof(struct bankAccount));
     
     strcpy((accountPtr)->accountHolder, (personPtr)->fullName);
-    printf("A back account was registered under the name of: %s", (accountPtr)->accountHolder);
+    printf("A back account was registered under the name of: %s\n\n", (accountPtr)->accountHolder);
     
+    while (clock() < 2000 * 1000);
+    printf("Would you like to withdraw or deposit?\n1: Deposit\n2: Withdraw\n\nI choose: ");
+    
+    int option, amt;
+    scanf("%d", &option);
+
+    if (option == 1) {
+        printf("How much would you like to deposit? (no commas)\n\nI would like to deposit: ");
+        scanf("%d", &amt);
+
+        if ((personPtr)->cash < amt) {
+            printf("You do not have enough cash in your wallet to deposit $%d.\n", amt);
+            return 0;
+        } else if (amt < 0) {
+            printf("Invalid deposit amount.\n");
+            return 0;
+        }
+
+        (accountPtr)->balance += amt;
+        (personPtr)->cash -= amt;
+
+        printf("Successfully deposited $%d into your account!\n\nYour new bank balance: %d\nAmount of cash in your wallet: %d\n", amt, (accountPtr)->balance, (personPtr)->cash);
+    } else if (option == 2) {
+        printf("How much would you like to withdraw? (no commas)\n\nI would like to withdraw: ");
+        scanf("%d", &amt);
+
+        if ((accountPtr)->balance < amt) {
+            printf("You do not have enough cash in your wallet to deposit $%d.\n", amt);
+            return 0;
+        } else if (amt < 0) {
+            printf("Invalid deposit amount.\n");
+            return 0;
+        }
+
+        (accountPtr)->balance -= amt;
+        (personPtr)->cash += amt;
+
+        printf("Successfully deposited $%d into your account!\n\nAmount of cash in your wallet: %d\nYour new bank balance: %d\n", amt, (personPtr)->cash, (accountPtr)->balance);
+    } else {
+        printf("That's not a valid option!");
+    }
+
     return 0;
 }
